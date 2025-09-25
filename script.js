b433d6a3b4dfd1e99654df91f5442a8ba093748c
Pavan Teja Reddy <142150313+PavanTejaReddy1@users.noreply.github.com>
2025-09-25 22:00:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-//your code here
+const divs = document.querySelectorAll(".image");
+
+divs.forEach(div => {
+	div.addEventListener("dragstart", (e)=>{
+		e.dataTransfer.setData("text", div.id);
+	})
+
+	div.addEventListener("dragover", (e)=>{
+		e.preventDefault();
+	})
+
+	div.addEventListener("drop", (e)=>{
+		e.preventDefault();
+
+		const dropedElement = e.target;
+		const dropedElementImage = getComputedStyle(dropedElement).backgroundImage;
+		const dropedElementTxt = dropedElement.textContent;
+
+		const draggedElement = document.getElementById(e.dataTransfer.getData("text"));
+		const draggedElementImage = getComputedStyle(draggedElement).backgroundImage;
+		const draggedElementTxt = draggedElement.textContent;
+
+		draggedElement.style.backgroundImage = dropedElementImage;
+		draggedElement.textContent = dropedElementTxt;
+
+		dropedElement.style.backgroundImage = draggedElementImage;
+		dropedElement.textContent = draggedElementTxt;
+	})
+})
