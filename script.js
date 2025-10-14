990be896b16307591386e01a3491e0d837b56665
Pavan Teja Reddy <142150313+PavanTejaReddy1@users.noreply.github.com>
2025-10-14 23:04:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
 const divs = document.querySelectorAll(".image");
 
 divs.forEach(div => {
-  div.addEventListener("dragstart", (e) => {
-    e.dataTransfer.setData("text", div.id);
-  });
+	div.addEventListener("dragstart", (e)=>{
+		e.dataTransfer.setData("text", div.id);
+	})
 
-  div.addEventListener("dragover", (e) => {
-    e.preventDefault();
-  });
+	div.addEventListener("dragover", (e)=>{
+		e.preventDefault();
+	})
 
-  div.addEventListener("drop", (e) => {
-    e.preventDefault();
+	div.addEventListener("drop", (e)=>{
+		e.preventDefault();
 
-    const droppedElement = e.target;
-    const droppedImage = getComputedStyle(droppedElement).backgroundImage;
-    const droppedText = droppedElement.textContent;
+		const dropedElement = e.target;
+		const dropedElementImage = getComputedStyle(dropedElement).backgroundImage;
+		const dropedElementTxt = dropedElement.textContent;
 
-    const draggedElement = document.getElementById(e.dataTransfer.getData("text"));
-    const draggedImage = getComputedStyle(draggedElement).backgroundImage;
-    const draggedText = draggedElement.textContent;
+		const draggedElement = document.getElementById(e.dataTransfer.getData("text"));
+		const draggedElementImage = getComputedStyle(draggedElement).backgroundImage;
+		const draggedElementTxt = draggedElement.textContent;
 
-    draggedElement.style.backgroundImage = droppedImage;
-    draggedElement.textContent = droppedText;
+		draggedElement.style.backgroundImage = dropedElementImage;
+		draggedElement.textContent = dropedElementTxt;
 
-    droppedElement.style.backgroundImage = draggedImage;
-    droppedElement.textContent = draggedText;
-  });
-});
+		dropedElement.style.backgroundImage = draggedElementImage;
+		dropedElement.textContent = draggedElementTxt;
+	})
+})
